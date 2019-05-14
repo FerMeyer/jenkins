5730509fd8fbab999b75a210f7a922bb130a57fa
ferme <ferme@DESKTOP-4ULEHVQ>
2019-05-14 14:26:42 -0300

    Meu terceiro commit

---
+++ src/matematicaa/numeros.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package matematicaa;
+
+/**
+ *
+ * @author ferme
+ */
+public class numeros {
+    
+    
+            public boolean numeroPar(int n){
+            return n%2==0;
+            
+        }
+        
+        public int  areaQuadrado( int lado){
+            return lado*lado;
+        }
+        
+        public boolean numeroDivisivel (int dividendo, int divisor){
+                return dividendo%divisor==0;
+        }
+        
+        
+        public int Inteiros(){
+               return 100;
+        }
+    
+}
---
+++ test/matematicaa/numerosTest.java
@@ -0,0 +1,60 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package matematicaa;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author ferme
+ */
+public class numerosTest {
+    
+     private numeros n;
+    
+    public numerosTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+         n = new numeros();
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    @Test
+    public void testNumeroPar() {
+        assertTrue(n.numeroPar(8));
+    }
+    
+    @Test
+    public void testAreaQuadrada() {
+        
+        assertEquals(100, n.areaQuadrado(10));
+    }
+    @Test
+    public void testDivisivel() {
+        
+        assertTrue(n.numeroDivisivel(4, 2));
+    
+    }
+    
+}
